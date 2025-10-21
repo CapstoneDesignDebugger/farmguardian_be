e214e08269883d7442ba3bd6448248067e8aed01
송현준 <70439776+SHJ0515@users.noreply.github.com>
2025-10-21 12:40:26 +0900

    refactor: Request DTO에 Setter 추가
    - DeviceConnectRequestDto에 @Setter 추가
    - FcmSendRequestDto에 @Setter 추가
    - FcmTokenRegisterRequestDto에 @Setter 추가
    - SignUpRequestDto에 @Setter 추가
    - JSON 역직렬화 및 테스트 지원

--- src/main/java/com/farmguardian/farmguardian/dto/request/DeviceConnectRequestDto.java
+++ src/main/java/com/farmguardian/farmguardian/dto/request/DeviceConnectRequestDto.java
@@ -3,10 +3,12 @@ package com.farmguardian.farmguardian.dto.request;
 import com.farmguardian.farmguardian.domain.TargetCrop;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class DeviceConnectRequestDto {
 
--- src/main/java/com/farmguardian/farmguardian/dto/request/FcmSendRequestDto.java
+++ src/main/java/com/farmguardian/farmguardian/dto/request/FcmSendRequestDto.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Map;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class FcmSendRequestDto {
